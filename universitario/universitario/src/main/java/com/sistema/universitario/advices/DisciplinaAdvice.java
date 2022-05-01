3b2169bd22c6d7b24ff32eda510258c1ef806a05
RafaelLopreti <rafael.lopreti@gmail.com>
2022-05-01 14:55:34 -0300

    Professor add log

--- universitario/universitario/src/main/java/com/sistema/universitario/advices/DisciplinaAdvice.java
+++ universitario/universitario/src/main/java/com/sistema/universitario/advices/DisciplinaAdvice.java
@@ -1,21 +1,30 @@
 package com.sistema.universitario.advices;
 
-import com.sistema.universitario.exceptions.CursoNaoEncontradoException;
 import com.sistema.universitario.exceptions.DisciplinaNaoEncontradaException;
-import lombok.extern.slf4j.Slf4j;
+import com.sistema.universitario.exceptions.DisciplinaProfessorNaoEncontradaException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@Slf4j
 @ControllerAdvice
 public class DisciplinaAdvice {
+
+    private final Logger log = LoggerFactory.getLogger(DisciplinaAdvice.class);
 
     @ExceptionHandler
     public ResponseEntity NotFoundDisciplina(DisciplinaNaoEncontradaException e){
         ResponseEntity response = new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+        log.error("error message Disciplina não encontrada!");
         return response;
     }
 
+    @ExceptionHandler
+    public ResponseEntity NotFoundDisciplinaProfessor(DisciplinaProfessorNaoEncontradaException e){
+        ResponseEntity response = new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+        log.error("error message Professor(a) nessa Disciplina não encontrado(a)!");
+        return response;
+    }
 }

56e80a165d05c77a006c6a89d706aee500c941f4
Wendel-Henrique-Matera <100135117+Wendel-Henrique-Matera@users.noreply.github.com>
2022-05-02 11:47:37 -0300

    Create table view in localhost:8080

--- universitario/universitario/src/main/java/com/sistema/universitario/controller/AlunoController.java
+++ universitario/universitario/src/main/java/com/sistema/universitario/controller/AlunoController.java
@@ -2,25 +2,36 @@ package com.sistema.universitario.controller;
 
 import com.sistema.universitario.models.Aluno;
 import com.sistema.universitario.repositories.AlunoRepository;
+import com.sistema.universitario.services.AlunoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping ("/aluno")
 public class AlunoController {
 
+
     private final AlunoRepository alunoRepository;
-    public AlunoController(AlunoRepository alunoRepository) {
+    public AlunoController(AlunoService alunoService, AlunoRepository alunoRepository) {
         this.alunoRepository = alunoRepository;
          }
 
-    @GetMapping
+    @GetMapping("todos-alunos")
     public List<Aluno> getAll(){
         return this.alunoRepository.findAll();
+    }
+
+    @GetMapping
+    public String index(Model model){
+        List<Aluno> alunos = getAll();
+        model.addAttribute("alunos", alunos);
+        return "index";
     }
 
     @GetMapping("/aluno-ativo")

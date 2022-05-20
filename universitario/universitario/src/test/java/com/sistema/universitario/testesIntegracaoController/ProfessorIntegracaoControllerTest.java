5e00c4077cda3f4cbb52cafb5811d7687308f5b5
RafaelLopreti <rafael.lopreti@gmail.com>
2022-05-20 12:04:57 -0300

    Conclude test ProfessorIntegracaoController and fix ProfessorIntegracaoService

--- universitario/universitario/src/test/java/com/sistema/universitario/testesIntegracaoController/ProfessorIntegracaoControllerTest.java
+++ universitario/universitario/src/test/java/com/sistema/universitario/testesIntegracaoController/ProfessorIntegracaoControllerTest.java
@@ -1,10 +1,117 @@
 package com.sistema.universitario.testesIntegracaoController;
 
+import com.sistema.universitario.models.Professor;
+import com.sistema.universitario.models.StatusUsuario;
+import com.sistema.universitario.services.EnderecoService;
+import com.sistema.universitario.services.ProfessorService;
+import com.sistema.universitario.services.UsuarioService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProfessorIntegracaoControllerTest {
 
+    @Autowired
+    TestRestTemplate restTemplate;
 
+    @Autowired
+    UsuarioService usuarioService;
 
+    @Autowired
+    EnderecoService enderecoService;
+
+    @Autowired
+    ProfessorService professorService;
+
+    @Test
+    void listarTodosProfessores() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpEntity<Professor> httpEntity = new HttpEntity<>(httpHeaders);
+
+        ResponseEntity<String> response = this.restTemplate
+                .exchange("/professor/listar-todos", HttpMethod.GET, httpEntity, String.class);
+
+        Assertions.assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void listarProfessoresAtivos() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpEntity<Professor> httpEntity = new HttpEntity<>(httpHeaders);
+
+        ResponseEntity<String> response = this.restTemplate
+                .exchange("/professor/listar-ativos", HttpMethod.GET, httpEntity, String.class);
+
+        Assertions.assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void encontrarProfessorPorId() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpEntity<Professor> httpEntity = new HttpEntity<>(httpHeaders);
+
+        ResponseEntity<String> response = this.restTemplate
+                .exchange("/professor/encontrar/1", HttpMethod.GET, httpEntity, String.class);
+
+        Assertions.assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void cadastrarProfessor() {
+        var catchUserId = usuarioService.findById(1L);
+        var catchEndId = enderecoService.findEnderecoById(1L);
+
+        Professor professor = new Professor();
+        professor.setUsuario(catchUserId);
+        professor.setNome("Integracao Controller");
+        professor.setCpf("123456");
+        professor.setEndereco(catchEndId);
+        professor.setStatus(StatusUsuario.ATIVO);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpEntity<Professor> httpEntity = new HttpEntity<>(professor, httpHeaders);
+
+        ResponseEntity<String> response = this.restTemplate
+                .exchange("/professor/cadastrar", HttpMethod.POST, httpEntity, String.class);
+
+        Assertions.assertNotNull(professor);
+        Assertions.assertEquals(201, response.getStatusCodeValue());
+    }
+
+    @Test
+    void atualizarProfessor() {
+        var professor = professorService.findById(1L);
+        professor.setNome("Integracao Controller Atualizado");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpEntity<Professor> httpEntity = new HttpEntity<>(professor, httpHeaders);
+
+        ResponseEntity<String> response = this.restTemplate
+                .exchange("/professor/atualizar/1", HttpMethod.PUT, httpEntity, String.class);
+
+        Assertions.assertNotNull(professor);
+        Assertions.assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void deletarProfessor() {
+        var professor = professorService.findById(1L);
+        professor.setStatus(StatusUsuario.INATIVO);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpEntity<Professor> httpEntity = new HttpEntity<>(professor, httpHeaders);
+
+        ResponseEntity<String> response = this.restTemplate
+                .exchange("/professor/deletar/1", HttpMethod.DELETE, httpEntity, String.class);
+
+        Assertions.assertNotNull(professor);
+        Assertions.assertEquals(200, response.getStatusCodeValue());
+    }
 }

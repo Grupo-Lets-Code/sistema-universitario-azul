d9ec7a5c4eb8034ff313356edbb878c9bc2acf79
RafaelLopreti <rafael.lopreti@gmail.com>
2022-05-23 13:53:58 -0300

    Add tests Disciplina

---
+++ universitario/universitario/src/test/java/com/sistema/universitario/testesUnitariosService/DisciplinaUnitarioServiceTest.java
@@ -0,0 +1,75 @@
+package com.sistema.universitario.testesUnitariosService;
+
+import com.sistema.universitario.models.*;
+import com.sistema.universitario.repositories.DisciplinaRepository;
+import com.sistema.universitario.repositories.ProfessorRepository;
+import com.sistema.universitario.services.DisciplinaService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+public class DisciplinaUnitarioServiceTest {
+
+    @InjectMocks
+    private DisciplinaService disciplinaService;
+
+    @Mock
+    private DisciplinaRepository disciplinaRepository;
+    @Mock
+    private ProfessorRepository professorRepository;
+
+    Professor professor;
+    Disciplina disciplina;
+
+    @BeforeEach
+    void initProfessor() {
+        professor = new Professor();
+    }
+
+    @BeforeEach
+    void initDisciplina() {
+        disciplina = new Disciplina();
+    }
+
+    @Test
+    @DisplayName("Teste listar todas - Disciplina")
+    void listarTodasDisciplinas() {
+        List<Disciplina> disciplinasList = new ArrayList<>();
+        disciplinasList.add(new Disciplina());
+
+        disciplinasList.add(new Disciplina());
+
+        Mockito.when(disciplinaRepository.findAll())
+                .thenReturn(disciplinasList);
+
+        List<Disciplina> disciplinas = disciplinaService.findAll();
+        Assertions.assertNotNull(disciplinas);
+        Assertions.assertEquals(2, disciplinasList.size());
+    }
+
+    @Test
+    @DisplayName("Teste deletar - Disciplina-Professor")
+    void deletarDisciplinaProfessor() {
+        Mockito.when(professorRepository.findById(professor.getId()))
+                .thenReturn(Optional.ofNullable(professor));
+
+        Mockito.when(disciplinaRepository.findById(disciplina.getId()))
+                .thenReturn(Optional.ofNullable(disciplina));
+
+        disciplinaService.deleteProfessorDisciplina(professor.getId(), disciplina.getId());
+
+        Assertions.assertNotNull(professor);
+        Assertions.assertNotNull(disciplina);
+    }
+}
